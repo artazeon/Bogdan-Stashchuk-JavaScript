4f31010871cfca387ae829004e1bf5b172b5203d
artazeon <artazeon@gmail.com>
2023-01-18 00:21:25 +0300

    JS Bogdan Stashchuk - Tempalate string

--- Bogdan Stashchuk/07 Template string literal.js
+++ Bogdan Stashchuk/07 Template string literal.js
@@ -1,3 +1,9 @@
-const a = 'Hello'
-const b = 'World'
-console.log(`${a} ${b}`)
+// const a = 'Hello'
+// const b = 'World'
+// console.log(`${a} ${b}`)
+
+
+const myName = 'Артём'
+const myCity = 'Солигорск'
+const full = 'Меня зовут ' + `${myName}` + ' и я живу в городе ' + `${myCity}`
+console.log(full)
