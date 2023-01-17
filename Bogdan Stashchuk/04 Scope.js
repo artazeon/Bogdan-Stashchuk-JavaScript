60f852516d31c764c8e663d6bf9b64714b6ad4e8
artazeon <artazeon@gmail.com>
2023-01-17 13:23:26 +0300

    JS Bogdan Stashchuk - Function

---
+++ Bogdan Stashchuk/04 Scope.js
@@ -0,0 +1,19 @@
+////////////////////////////////
+// function myFn (a, b) {
+//     a++
+//     c = b + a
+//     return c
+// }
+
+////////////////////////////////
+//Кол бэк функция
+
+// function printMyName () {
+//     console.log('Artiom')
+// }
+// console.log('Start')
+// setTimeout (printMyName, 2000)
+
+////////////////////////////////
+//
+
