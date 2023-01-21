3dc793a71f9cfb9dae99245b389fdf9cfe1187a9
artazeon <artazeon@gmail.com>
2023-01-21 13:39:22 +0300

    nikita

---
+++ Bogdan Stashchuk/1.js
@@ -0,0 +1,26 @@
+let startLoad = 'step--xyu'
+
+const stepCount = () => {
+    let startLoadArr = startLoad.split('--')
+    let countNumberFirstPart = startLoadArr[0]
+    let countNumberSecondPart = startLoadArr[1]
+
+    
+    if (Number(countNumberSecondPart)) {
+        // console.log('число')
+        newCount = countNumberSecondPart++
+        console.log((countNumberFirstPart) + '--' + countNumberSecondPart)
+        // console.log(typeof (countNumberSecondPart))
+    
+}
+    else {console.log('не число')
+    console.log((countNumberFirstPart) + '--' + 1)
+} 
+
+} 
+
+stepCount()
+
+
+
+
