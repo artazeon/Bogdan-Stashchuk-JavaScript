37de2f15442cf265b56475bd636ef38499a085a8
artazeon <artazeon@gmail.com>
2023-01-23 20:25:24 +0300

    JS Bogdan Stashchuk - Деструктуризация

---
+++ Bogdan Stashchuk/14 Деструктуризация.js
@@ -0,0 +1,41 @@
+//////////// Объекты
+
+// const userProfile = {
+//     name: 'Artiom',
+//     commentsQty: 23,
+//     hasSignedAgreement: false,
+// }
+
+// const { name, commentsQty} = userProfile
+// const { hasSignedAgreement} = userProfile
+
+// console.log(name)
+// console.log(commentsQty)
+// console.log(hasSignedAgreement)
+
+////////////////////////////////////////////////////////
+//////////// Массивы 
+
+// const fruits = ['Apple', 'Banana']
+// const [fruitOne, fruitTwo] = fruits
+
+// console.log(fruitOne)
+// console.log(fruitTwo)
+
+////////////////////////////////////////////////////////
+//////////// Деструктуризация в функциях
+
+const userProfile = {
+    name: 'Artiom',
+    commentsQty: 23,
+    hasSignedAgreement: false,
+}
+
+const userInfo = ({ name, commentsQty}) => {
+    if (!commentsQty) {
+        return `User ${name} has no commenst`
+    }
+    return `User ${name} has ${commentsQty} commenst`
+}
+
+userInfo(userProfile)
