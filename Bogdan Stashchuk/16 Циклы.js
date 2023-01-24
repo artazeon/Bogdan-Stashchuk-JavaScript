4b1559ceaa85c6513e1291ecb68c914772e090e5
artazeon <artazeon@gmail.com>
2023-01-24 19:28:46 +0300

    JS Bogdan Stashchuk - Циклы

---
+++ Bogdan Stashchuk/16 Циклы.js
@@ -0,0 +1,26 @@
+// for
+// for ... in ...
+// while
+// do ... while
+// for ... off ...
+
+//////////////////// For
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+
+//////////////////// For для массивов
+// const myArray = ['first', 'second', 'third']
+
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(myArray[i])
+// }
+
+
+//////////////////// ForEach
+const myArray = ['first', 'second', 'third']
+
+myArray.forEach((element, index) => {
+    console.log(element, index)
+})
