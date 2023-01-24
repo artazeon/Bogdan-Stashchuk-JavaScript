3af95f333e9cc527525eebef4d01e9842ee23fa5
artazeon <artazeon@gmail.com>
2023-01-24 14:13:59 +0300

    JS Bogdan Stashchuk - Условные инструкции

---
+++ Bogdan Stashchuk/15 Условные инструкции.js
@@ -0,0 +1,57 @@
+// let val = 10
+
+// if (val > 5) {
+//     val +=20
+// }
+
+// console.log(val)
+
+///////////////////////////////////////////
+
+// const person = {
+//     age: 20
+// }
+
+// if (!person.name) {
+//     console.log('Имя не указано')
+// }
+
+///////////////////////////////////////////
+
+// let val = 10
+
+// if (val < 5) {
+//     val +=20
+// } else {
+//     val -=20
+// }
+
+// console.log(val)
+
+///////////////////////////////////////////
+
+// const age = 7
+
+// if (age > 18) {
+//     console.log('Is adult')
+// } else if ( age >= 12) {
+//     console.log('Is teenager')
+// } else {
+//     console.log('Is child')
+// }
+
+///////////////////////////////////////////
+
+const age = 17
+
+if (age > 18) {
+    console.log('Is adult')
+}
+
+if ( age >= 12 && age < 18) {
+    console.log('Is teenager')
+}
+
+if ( age < 12) {
+    console.log('Is child')
+}
