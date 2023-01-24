eae09b741ac4a7bf170f20d484ff77ad82f315a0
artazeon <artazeon@gmail.com>
2023-01-25 02:17:43 +0300

    JS Bogdan Stashchuk - Import-Export

--- Bogdan Stashchuk/modules/modelOne.mjs
+++ Bogdan Stashchuk/modules/modelOne.mjs
@@ -1,3 +1,24 @@
-const sum = (a, b) => a + b
+// const sum = (a, b) => a + b
 
-export default sum
+
+// export default sum
+
+
+///////////////////////////////////////
+
+// const one = 1
+// const two = 'two'
+
+// export {
+//     one,
+//     two
+// }
+
+///////////////////////////////////////
+const sum = (a, b) => a + b
+const mult = (a, b) => a * b
+
+export {
+    sum,
+    mult
+}
--- Bogdan Stashchuk/modules/modelTwo.mjs
+++ Bogdan Stashchuk/modules/modelTwo.mjs
@@ -1,5 +1,21 @@
-import sumNumbers from "./modelOne.mjs";
+// import sumNumbers from "./modelOne.mjs";
 
-const res = sumNumbers(10, 2)
+// const res = sumNumbers(10, 2)
 
-console.log(res)
+// console.log(res)
+
+///////////////////////////////////////
+
+// import {
+//     one as oneRenamed,
+//     two
+// } from './modelOne.mjs'
+
+// console.log(oneRenamed, two)
+
+///////////////////////////////////////
+
+import { sum, mult as multNumbers} from './modelOne.mjs'
+
+console.log(sum(10,2))
+console.log(multNumbers(10,2))
