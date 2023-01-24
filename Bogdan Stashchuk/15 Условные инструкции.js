0ea78d9e497bc98dc633399eafe6ea04140dbf71
artazeon <artazeon@gmail.com>
2023-01-24 14:56:02 +0300

    JS Bogdan Stashchuk - Условные инструкции

--- Bogdan Stashchuk/15 Условные инструкции.js
+++ Bogdan Stashchuk/15 Условные инструкции.js
@@ -42,16 +42,75 @@
 
 ///////////////////////////////////////////
 
-const age = 17
+// const age = 17
 
-if (age > 18) {
-    console.log('Is adult')
-}
+// if (age > 18) {
+//     console.log('Is adult')
+// }
 
-if ( age >= 12 && age < 18) {
-    console.log('Is teenager')
-}
+// if ( age >= 12 && age < 18) {
+//     console.log('Is teenager')
+// }
 
-if ( age < 12) {
-    console.log('Is child')
+// if ( age < 12) {
+//     console.log('Is child')
+// }
+
+///////////////////////////////////////////
+
+// Использование if в функциях
+
+// const sumPositiveNumber = (a, b) => {
+//     if (typeof a !== 'number' || typeof b !== 'number' ) {
+//         return 'One of the arguments is not a number'
+//     }
+
+//     if ( a < 0 || b < 0) {
+//         return 'Number is not a positive'
+//     }
+
+//     console.log (a + b)
+//     return a + b
+    
+// }
+
+// sumPositiveNumber (2, 5)
+
+///////////////////////////////////////////
+
+// Использование switch
+
+// switch () {
+//     case A:
+        
+//     break
+
+//     case B:
+
+//     break
+
+//     default: 
+// }
+
+///////////////////////////////////////////
+
+// Использование switch
+
+const month = 2
+
+switch (month) {
+    case 12:
+        console.log('Декабрь')
+    break
+        
+    case 1:
+        console.log('Январь')
+    break
+
+    case 2: 
+        console.log('Ферваль')
+    break
+    
+    default: 
+        console.log('Это не зимний месяц')
 }
