fe81b94a8adfce2e029d83b61972cd582f04aa35
artazeon <artazeon@gmail.com>
2023-02-02 16:36:38 +0300

    Nikita 3

---
+++ Nikita/3.js
@@ -0,0 +1,33 @@
+const company = {
+    production: {
+        team: {
+            front: [120, 160, 130, 10, 10]
+            }
+        }
+    }
+
+
+const arr = company.production.team.front
+
+console.log(arr)
+
+
+
+function maxHour(yarikHour2022)  {
+    let maxN = 0
+    const minN = arr[0]
+    yarikHour2022.every(elem => elem > maxN ? maxN = elem : true) 
+    
+     console.log(maxN)
+}
+
+maxHour(arr)
+
+
+// let minHour = Math.min(...arr);
+// let middleHour = (maxHour + minHour) / 2
+// console.log(maxHour)
+// console.log(minHour)
+// console.log(middleHour)
+
+
