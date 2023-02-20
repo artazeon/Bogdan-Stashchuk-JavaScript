f04696c12aeb55dc0cac4816436cf94767f88624
artazeon <artazeon@gmail.com>
2023-02-20 21:54:11 +0300

    Bogdan Stashchuk Practice 62

--- Bogdan Stashchuk/Practice/62-different-functions/finish.js
+++ Bogdan Stashchuk/Practice/62-different-functions/finish.js
@@ -7,10 +7,19 @@
  * 3. Также вызовите обе функции
  */
 
+// console.log(firstFunction (1, 2))
 function firstFunction(a, b) {
   return a + b
 }
+// firstFunction = 'asdas'
+
+// console.log(secondFunction (1, 2))
 
 const secondFunction = function (a, b) {
   return a + b
 }
+
+// secondFunction = true
+
+console.log(firstFunction (1, 2))
+console.log(secondFunction (1, 2))
