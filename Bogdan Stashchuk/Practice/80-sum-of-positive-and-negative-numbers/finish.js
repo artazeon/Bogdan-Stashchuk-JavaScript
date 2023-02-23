eec08296fbd735fd8371a984ee14db60dd374752
artazeon <artazeon@gmail.com>
2023-02-23 13:51:47 +0300

    Bogdan Stashchuk Practice 80

--- Bogdan Stashchuk/Practice/80-sum-of-positive-and-negative-numbers/finish.js
+++ Bogdan Stashchuk/Practice/80-sum-of-positive-and-negative-numbers/finish.js
@@ -12,6 +12,37 @@
 
 const nums = [10, -12, 30, -1, -8, 0, 14, -33, 20]
 
+// const sumPositiveNegative = (arr) => {
+//     return arr.reduce((accum, item)=> {
+//         if (item > 0) {
+//             return {
+//                 ...accum,
+//                 positive: accum.positive + item,
+//             }
+//         }
+
+//         return {
+//             ...accum,
+//             negative: accum.negative + item,
+//         }
+//     }, {positive: 0, negative: 0})
+    
+// }
+
+
+const sumPositiveNegative = (arr) => {
+    return arr.reduce((accum, item)=> {
+        let type = item > 0 ? 'positive' : 'negative'
+        
+        return {
+            ...accum,
+            [type]: accum[type] + item,
+        }
+    }, {positive: 0, negative: 0})
+    
+}
+
+
 // Создайте функцию "sumPositiveNegative" здесь
 
 const result = sumPositiveNegative(nums)
